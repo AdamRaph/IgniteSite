6be5378df3d37e7de31a87357308ab78f8778ccd
Adam Raphael <adam.raphael@hotmail.com>
2017-02-03 10:18:34 +1100

    Files

---
+++ IgniteSite/App_Code/Attendee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Table;
+
+/// <summary>
+/// Summary description for Attendees
+/// </summary>
+
+public class Attendee : TableEntity
+{
+    public Attendee()
+    {
+    }
+    public Attendee(string id)
+    {
+        this.PartitionKey = "Attendee";
+        this.RowKey = id;
+    }
+    public int memberID { get; set; }
+    public string name { get; set; }
+    public string company { get; set; }
+    public string jobTitle { get; set; }
+    public string ticketType { get; set; }
+    public int ticketQuantity { get; set; }
+}
---
+++ IgniteSite/App_Code/TableConn.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+/// <summary>
+/// Summary description for TableConn
+/// </summary>
+/// 
+
+
+public class TableConn
+{
+    public TableConn()
+    {
+        storageAcc = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=adamdevstorage;AccountKey=ni7ZRTfgiaCuWbOqrSVoIZADMDoOi1/riCvbdu/76NmGbOI5M0jN1i2w/Q84xhRsAQgjti9Vwrmgh6JU9Nlxiw==;");
+        tableClient = storageAcc.CreateCloudTableClient();
+        table = tableClient.GetTableReference("IgniteAttendees");
+    }
+
+    public IEnumerable<Attendee> retrieve(string name)
+    {
+        var query = new TableQuery<Attendee>().Where(TableQuery.GenerateFilterCondition(nameof(Attendee.name), QueryComparisons.Equal, name));
+        IEnumerable<Attendee> result = table.ExecuteQuery(query);
+        if (result.Count() == 0)
+        {
+            return null;
+        }
+        //need some logic for people w/ same name
+        else
+            return result;
+    }
+
+    private CloudStorageAccount storageAcc;
+    private CloudTableClient tableClient;
+    private CloudTable table;
+}
